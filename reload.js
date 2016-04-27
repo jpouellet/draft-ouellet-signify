48d4bd09706a42b47bc7ccda0f59d900549f9d2c
Jean-Philippe Ouellet <jpo@vt.edu>
2016-04-27 16:33:28 -0400

    Improve page reloading.

--- reload.js
+++ reload.js
@@ -9,8 +9,10 @@ window.addEventListener('load', function() {
 (function(){
 		var xhr = new XMLHttpRequest();
 		xhr.onreadystatechange = function() {
-			if (xhr.readyState === 4)
+			if (xhr.readyState === 4) {
+				console.log('reload triggered for '+xhr.responseText);
 				location.reload();
+			}
 		};
 		xhr.open('GET', '/wait.cgi', true);
 		xhr.send();
--- wait.cgi
+++ wait.cgi
@@ -3,5 +3,8 @@
 echo 'Content-Type: text/plain'
 echo
 
-# could use some better debouncing here
-inotifywait -q -e close_write --exclude '.*~' --format '' .
+inotifywait -q -e close_write --exclude '/(\..+|.*~)' --format '%f' .
+# I wish there were an --include whitelist instead of just an --exclude.
+
+sleep 0.05
+# Debouncing could be more robust.
